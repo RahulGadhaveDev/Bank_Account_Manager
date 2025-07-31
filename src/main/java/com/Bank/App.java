e6660d63daba3044cad387c04cb2315c84932139
Rahul Gadhave <rahulgadhave022@gmail.com>
2025-07-31 23:46:25 +0530

    add some

--- src/main/java/com/Bank/App.java
+++ src/main/java/com/Bank/App.java
@@ -8,27 +8,24 @@ import com.Bank.repository.RepoBank;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	RepoBank repo = new RepoBank();
-    	
-    	System.out.println("1. post data");
-    	System.out.println("2. Enter id to get account ");
-    	Scanner sc = new Scanner(System.in);
-    	
-    	int opration = sc.nextInt();
-    	
-    	switch (opration) {
-    	case 1:
-    		repo.postData();
-    		break;
-    	case 2: 
-    		repo.getData();
-    		break;
-    	}
-    	
-    	
-    }
+public class App {
+	public static void main(String[] args) {
+		RepoBank repo = new RepoBank();
+
+		System.out.println("1. post data");
+		System.out.println("2. Enter id to get account ");
+		Scanner sc = new Scanner(System.in);
+
+		int opration = sc.nextInt();
+
+		switch (opration) {
+		case 1:
+			repo.postData();
+			break;
+		case 2:
+			repo.getData();
+			break;
+		}
+
+	}
 }
