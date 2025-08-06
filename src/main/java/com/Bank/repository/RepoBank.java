5fac38050dde7257f8c6de3e6cddfd07974a7e23
Rahul Gadhave <rahulgadhave022@gmail.com>
2025-08-06 22:45:34 +0530

    add some

--- src/main/java/com/Bank/repository/RepoBank.java
+++ src/main/java/com/Bank/repository/RepoBank.java
@@ -12,7 +12,7 @@ import com.Bank.utility.BankUtility;
 public class RepoBank {
 	Scanner sc = new Scanner(System.in);
 	
-	
+	//post data from data
 	public void postData() {
 
 		Account account = new Account();
