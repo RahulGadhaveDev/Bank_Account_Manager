efe11c3f6701af52f54c122d23a8a580426a58a1
Rahul Gadhave <rahulgadhave022@gmail.com>
2026-03-21 23:59:07 +0530

    Update Account.java

--- src/main/java/com/Bank/Entity/Account.java
+++ src/main/java/com/Bank/Entity/Account.java
@@ -8,7 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-//entity class in 
+//entity class
 
 @Entity
 @Table(name = "Accounts")
